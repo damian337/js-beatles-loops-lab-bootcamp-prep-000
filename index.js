a54976515226efffd828a92aa128735aab3f95f8
Damian Guillory <guillorywdamian@gmail.com>
2017-10-11 01:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
     var array = [];
     for (i = 0; i < musicians.length; i++) {
-        
+        array.push(`${musicians[i]}` + `${instruments[i]}`)
     }
 }
